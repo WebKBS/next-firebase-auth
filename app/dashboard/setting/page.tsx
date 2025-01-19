301553a94a63b590ebbd2f0c1c8826ad1144e6b9
kbs21c1987 <kbs21c1987@gmail.com>
2025-01-19 16:08:40 +0900

    feat: dynamic 추가

--- app/dashboard/setting/page.tsx
+++ app/dashboard/setting/page.tsx
@@ -1,8 +1,17 @@
-const SettingPage = () => {
+export const dynamic = "force-dynamic";
+
+const SettingPage = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
   return (
     <div>
       <h1>Setting</h1>
       <div>설정 페이지</div>
+      <div>
+        {data.map((item: { id: number; title: string }) => (
+          <div key={item.id}>{item.title}</div>
+        ))}
+      </div>
     </div>
   );
 };
